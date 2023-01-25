9c5ee702918f0b0ea280a04ff4f5256d3aab6d62
Naimur Rahman <naimur2231@gmail.com>
2023-01-25 23:30:34 +0600

    Mathematical operations

---
+++ arithmetic_operations.js
@@ -0,0 +1,23 @@
+// addition & subtraction
+var applePrice = 50;
+var orangePrice = 30;
+
+var totalPrice = applePrice + orangePrice ;
+console.log(totalPrice);
+
+var differentPrice = applePrice - orangePrice ;
+console.log(differentPrice);
+
+// multiplication 
+var price = 10;
+var quantity = 5;
+
+var netPrice = price * quantity;
+console.log(netPrice);
+
+// division
+var amount = 600;
+var player = 6;
+
+var singlePlayer = amount / player;
+console.log(singlePlayer);
