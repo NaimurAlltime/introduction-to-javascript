84686325e14eebd4b6a1916de5c8399f12a8fb76
Naimur Rahman <naimur2231@gmail.com>
2023-01-26 21:53:23 +0600

    shorthand practice

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+var price1 = 10;
+price1 += 8;
+console.log(price1);
+
+price1 -= 5;
+console.log(price1);
+
+price1++;
+console.log(price1);
+
+price1--;
+console.log(price1);
+
+var remainder = price1 % 3;
+console.log(remainder);
