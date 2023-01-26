a69e288748bc19968b7507462b820df1a340c348
Naimur Rahman <naimur2231@gmail.com>
2023-01-26 22:05:11 +0600

    conversion js

---
+++ convert.js
@@ -0,0 +1,16 @@
+var price1 = 20;
+var price2 = 10.5;
+
+var totalPrice = price1 + price2;
+var updatePrice = parseInt(totalPrice);
+
+console.log(totalPrice);
+console.log(updatePrice);
+
+var num1 = 2.554;
+var num2 = 3.52;
+var sum =  num1 + num2;
+console.log(sum);
+
+var updateSum = sum.toFixed(2);
+console.log(updateSum);
